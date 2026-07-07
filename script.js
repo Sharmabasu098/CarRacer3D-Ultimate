6a8bf1a47ac245e1e5a0011750c1413ddf6cf24a
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-07 06:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
+import * as THREE from "three";
 
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87ceeb);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 3;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+
+renderer.setSize(
+  window.innerWidth,
+  window.innerHeight
+);
+
+renderer.setPixelRatio(
+  window.devicePixelRatio
+);
+
+document.body.appendChild(
+  renderer.domElement
+);
+
+// Cube
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshNormalMaterial()
+);
+
+scene.add(cube);
+
+// Animation
+function animate() {
+
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+
+}
+
+animate();
+
+// Resize
+window.addEventListener("resize", () => {
+
+  camera.aspect =
+    window.innerWidth /
+    window.innerHeight;
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+  );
+
+});
