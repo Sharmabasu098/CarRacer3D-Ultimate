9229569dcecf911d151eeef7e0332f4f20e08914
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-08 07:52:31 +0530

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,39 @@
+import * as THREE from "three";
 
+export let player;
+
+export function createPlayer(scene) {
+
+    player = new THREE.Mesh(
+
+        new THREE.BoxGeometry(1.5, 1, 3),
+
+        new THREE.MeshLambertMaterial({
+            color: 0xff0000
+        })
+
+    );
+
+    player.position.set(0, 0.5, 4);
+
+    scene.add(player);
+
+}
+
+export function updatePlayer(moveLeft, moveRight) {
+
+    const speed = 0.15;
+
+    if (moveLeft && player.position.x > -2.8) {
+
+        player.position.x -= speed;
+
+    }
+
+    if (moveRight && player.position.x < 2.8) {
+
+        player.position.x += speed;
+
+    }
+
+}
