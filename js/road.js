0bb17674aa1c7ef3c73807c6a9f3702020bd69cf
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-08 07:48:15 +0530

    Update road.js

--- js/road.js
+++ js/road.js
@@ -1,1 +1,85 @@
+import * as THREE from "three";
 
+export let laneLines = [];
+
+export function createRoad(scene) {
+
+    // Ground
+    const ground = new THREE.Mesh(
+
+        new THREE.PlaneGeometry(200, 200),
+
+        new THREE.MeshLambertMaterial({
+            color: 0x3fa34d
+        })
+
+    );
+
+    ground.rotation.x = -Math.PI / 2;
+    scene.add(ground);
+
+    // Road
+    const road = new THREE.Mesh(
+
+        new THREE.PlaneGeometry(8, 200),
+
+        new THREE.MeshLambertMaterial({
+            color: 0x333333
+        })
+
+    );
+
+    road.rotation.x = -Math.PI / 2;
+    road.position.y = 0.01;
+
+    scene.add(road);
+
+    // Lane Lines
+    const laneMaterial =
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff
+        });
+
+    laneLines = [];
+
+    for (let i = 0; i < 40; i++) {
+
+        const line = new THREE.Mesh(
+
+            new THREE.PlaneGeometry(0.2, 2),
+
+            laneMaterial
+
+        );
+
+        line.rotation.x = -Math.PI / 2;
+
+        line.position.set(
+            0,
+            0.02,
+            -i * 5
+        );
+
+        scene.add(line);
+
+        laneLines.push(line);
+
+    }
+
+}
+
+export function updateRoad() {
+
+    laneLines.forEach(line => {
+
+        line.position.z += 0.5;
+
+        if (line.position.z > 10) {
+
+            line.position.z = -190;
+
+        }
+
+    });
+
+}
