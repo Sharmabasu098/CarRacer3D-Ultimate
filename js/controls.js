949d8d3a1e9da3ef0967a9fd993fab58af3a7424
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-08 07:56:22 +0530

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,1 +1,27 @@
+export let moveLeft = false;
+export let moveRight = false;
 
+export function setupControls() {
+
+    window.addEventListener("touchstart", (e) => {
+
+        if (e.touches[0].clientX < window.innerWidth / 2) {
+
+            moveLeft = true;
+
+        } else {
+
+            moveRight = true;
+
+        }
+
+    });
+
+    window.addEventListener("touchend", () => {
+
+        moveLeft = false;
+        moveRight = false;
+
+    });
+
+}
