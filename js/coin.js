84ac6c0e183c8c5090349c009a60c67aec82ae6a
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-12 22:11:25 +0530

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -63,8 +63,8 @@ export function collectCoins() {
 
         if (
 
-            Math.abs(coin.position.x - player.position.x) < 1.5 &&
-            Math.abs(coin.position.z - player.position.z) < 2.5
+            Math.abs(coin.position.x - player.position.x) < 1 &&
+            Math.abs(coin.position.z - player.position.z) < 2
 
         ) {
 
