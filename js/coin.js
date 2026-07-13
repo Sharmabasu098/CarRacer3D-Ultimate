73776dc1125ca1c2e8415330b3b4a8b1c7f38408
Sharmabasu098 <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-13 08:15:29 +0530

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -22,20 +22,34 @@ export function createCoins(scene) {
 
         );
 
-        export function updateCoins() {
+        coin.rotation.z = Math.PI / 2;
+
+        coin.position.set(
+            lanes[Math.floor(Math.random() * 3)],
+            1,
+            -20 - (i * 25)
+        );
+
+        scene.add(coin);
+        coins.push(coin);
+
+    }
+
+}
+
+export function updateCoins() {
 
     coins.forEach(coin => {
 
-        // Rotate Coin
+        // Spin
         coin.rotation.x += 0.15;
 
-        // Move Coin
+        // Move
         coin.position.z += 0.15;
 
         if (coin.position.z > 12) {
 
             coin.position.z = -180;
-
             coin.position.x =
                 lanes[Math.floor(Math.random() * 3)];
 
@@ -50,17 +64,13 @@ export function collectCoins() {
     coins.forEach(coin => {
 
         if (
-
             Math.abs(coin.position.x - player.position.x) < 2 &&
             Math.abs(coin.position.z - player.position.z) < 3
-
         ) {
 
             coinCount++;
 
-            // Respawn Coin
             coin.position.z = -180 - Math.random() * 40;
-
             coin.position.x =
                 lanes[Math.floor(Math.random() * 3)];
 
