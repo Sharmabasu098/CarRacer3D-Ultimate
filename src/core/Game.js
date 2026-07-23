e99b5bc39718164d281418a248340b183d72001f
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-23 16:41:15 +0530

    Update Game.js

--- src/core/Game.js
+++ src/core/Game.js
@@ -1,1 +1,45 @@
+import SceneManager from "./SceneManager.js";
+import Renderer from "./Renderer.js";
+import Clock from "./Clock.js";
 
+export default class Game {
+
+    constructor() {
+
+        this.sceneManager = new SceneManager();
+        this.renderer = new Renderer();
+        this.clock = new Clock();
+
+        this.scene = this.sceneManager.getScene();
+        this.camera = this.sceneManager.getCamera();
+
+        this.animate = this.animate.bind(this);
+
+    }
+
+    start() {
+
+        this.animate();
+
+    }
+
+    animate() {
+
+        requestAnimationFrame(this.animate);
+
+        const delta = this.clock.getDelta();
+
+        // Future Update Systems
+        // Player
+        // Traffic
+        // Road
+        // Coins
+        // Camera
+        // UI
+        // Audio
+
+        this.renderer.render(this.scene, this.camera);
+
+    }
+
+}
