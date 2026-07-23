0fc208f28db1a749debfb2eda588ea64577cf027
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-23 16:28:56 +0530

    Update Clock.js

--- src/core/Clock.js
+++ src/core/Clock.js
@@ -1,1 +1,23 @@
+import * as THREE from "three";
 
+export default class Clock {
+
+    constructor() {
+
+        this.clock = new THREE.Clock();
+
+    }
+
+    getDelta() {
+
+        return this.clock.getDelta();
+
+    }
+
+    getElapsedTime() {
+
+        return this.clock.getElapsedTime();
+
+    }
+
+}
