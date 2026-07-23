56c7a7f8ceba0877b812b0bb1a87bce911f079e8
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-23 16:26:36 +0530

    Update Renderer.js

--- src/core/Renderer.js
+++ src/core/Renderer.js
@@ -1,1 +1,41 @@
+import * as THREE from "three";
 
+export default class Renderer {
+
+    constructor() {
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: false
+        });
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        document.body.appendChild(this.renderer.domElement);
+
+    }
+
+    getRenderer() {
+
+        return this.renderer;
+
+    }
+
+    resize() {
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+    }
+
+}
