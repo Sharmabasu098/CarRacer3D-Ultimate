c34d0b3c8fb68c2c920e44fb61cc4efe3c505191
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-23 16:22:14 +0530

    Update SceneManager.js

--- src/core/SceneManager.js
+++ src/core/SceneManager.js
@@ -1,1 +1,23 @@
+import * as THREE from "three";
 
+export default class SceneManager {
+
+    constructor() {
+
+        this.scene = new THREE.Scene();
+
+        // Background Color
+        this.scene.background = new THREE.Color(0x87CEEB);
+
+        // Fog
+        this.scene.fog = new THREE.Fog(0x87CEEB, 30, 120);
+
+    }
+
+    getScene() {
+
+        return this.scene;
+
+    }
+
+}
